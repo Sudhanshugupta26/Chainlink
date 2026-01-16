1536b15bd69c70503997f4d2bc6d499533283a80
Sudhanshugupta26 <guptasudhanshukumar1@gmail.com>
2026-01-16 18:17:21 +0530

    Code Push

---
+++ contracts/CCIP/CCIP chainlink local/CCIPLocalSimulator.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+// solhint-disable no-unused-import
+import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
+
+contract CCIPLocal is CCIPLocalSimulator{
+
+}
